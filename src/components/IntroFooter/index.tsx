a08d2dc4f360cfded463fb518554356cfd4db59a
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-16 07:19:06 +0100

    Style tracker

--- src/components/IntroFooter/index.tsx
+++ src/components/IntroFooter/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import { Container } from './styled';
+import { Container, Tracker, Circle } from './styled';
 import Icon from '../Icon';
 
 
@@ -13,11 +13,19 @@ const IntroFooter = ({ navigation }) => {
   const nameOfScreen = index => navigation.state.routes[index].routeName;
   const navigateToNextScreen = () => indexOfCurrentScreen !== 2 ? navigation.navigate(nameOfScreen(indexOfCurrentScreen + 1))
 : navigation.navigate('Home');
+  const trackerColor = indexOfComp => indexOfCurrentScreen ===   indexOfComp;
   return (
     <Container>
       <TouchableOpacity onPress={goHome}>
         <Icon iconName="home" />
       </TouchableOpacity>
+      <Tracker>
+        {
+          [0, 1, 2].map(indexOfComp => (
+            <Circle key={indexOfComp} isActive={trackerColor(indexOfComp)} />
+          ))
+        }
+      </Tracker>
       <TouchableOpacity onPress={navigateToNextScreen}>
         <Icon iconName={indexOfCurrentScreen === 2 ? 'check' : 'arrowright'} />
       </TouchableOpacity>
